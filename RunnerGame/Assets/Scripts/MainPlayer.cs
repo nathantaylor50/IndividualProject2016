bcb3c61ea2cf8ed651bb542708ce6a21b50fc196
nathantaylor50 <nathantaylor50@msn.com>
2016-02-09 10:52:24 +0000

    push test

---
+++ RunnerGame/Assets/Scripts/MainPlayer.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+//namespace = collection
+namespace RunnerGame
+{
+    /*
+        This class is for having the player character switch lanes by moving left or right
+    */ 
+    ///  
+    public class MainPlayer : MonoBehaviour
+    {
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
