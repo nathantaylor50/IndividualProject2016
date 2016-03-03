560ff60d91694ca754afc137a1c559e99330e54c
nathantaylor50 <nathantaylor50@msn.com>
2016-03-03 16:40:36 +0000

    test

---
+++ RunnerGame/Assets/Scripts/GameManager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+    //singleton pattern
+    static public GameManager Instance { get { return _instance; } }
+    static protected GameManager _instance;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    //the various states the game can be in
+    public enum Gamestatus { BeforeGameStart, GameInProgress, Paused, GameOver, LifeLost};
+    //the current status of the game
+    public Gamestatus Status { get; protected set; }
+
+    public virtual void Pause() { }
+
+    public virtual void UnPause() { }
+}
---
+++ RunnerGame/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+/*
+    t
+*/
+public class LevelManager : MonoBehaviour {
+
+    //singleton pattern
+    static public LevelManager Instance { get { return _instance; } }
+    static protected LevelManager _instance;
+
+    protected virtual void Awake()
+    {
+        _instance = this;
+    }
+
+    public enum Controls { SingleButton, LeftRight, Jump }
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+    [Space(10)]
+    [Header("Mobile Controls")]
+    //the mobile control scheme applied to this level
+    public Controls ControlScheme;
+
+    /*
+        Triggered when all lives are lost and the player presses the main action button down
+    */ 
+    public virtual void GameOverAction()
+    {
+        //Application.LoadLevel(Application.LoadedLevel);
+    }
+
+    //
+    public virtual void LifeLostAction()
+    {
+        
+    }
+}
+
+
