6c50c44be9a86a1ef55f6271b88c62121b2cdbc2
nathantaylor50 <nathantaylor50@msn.com>
2016-04-11 19:15:21 +0100

    Event Dictionary

--- RunnerGame/Assets/Scripts/EventManager.cs
+++ RunnerGame/Assets/Scripts/EventManager.cs
@@ -1,11 +1,60 @@
-﻿using System;
+﻿using UnityEngine;
+using UnityEngine.Events;
+using System.Collections;
+using System.Collections.Generic;
 
-namespace AssemblyCSharp
+namespace RunnerGame
 {
-	public class EmptyClass
+	/// <summary>
+	/// Event manager.
+	/// </summary>
+	public class EventManager
 	{
-		public EmptyClass ()
+		/// <summary>
+		/// The event dictionary.
+		/// </summary>
+		private static Dictionary <string, UnityEvent> eventDictionary;
+
+		/// <summary>
+		/// Init this instance.
+		/// </summary>
+		private static void init()
 		{
+			if (eventDictionary == null) {
+				eventDictionary = new Dictionary<string, UnityEvent> ();
+			}
+		}
+
+		public static void StartListener (string eventName,  UnityAction listener)
+		{
+			init ();
+			UnityEvent thisEvent = null;
+			if (eventDictionary.TryGetValue (eventName, out thisEvent)) {
+				thisEvent.AddListener (listener);
+			} else {
+				thisEvent = new UnityEvent ();
+				thisEvent.AddListener (listener);
+				eventDictionary.Add (eventName, thisEvent);
+			}
+		}
+
+		public static void StopListener (string eventName, UnityAction listener)
+		{
+			init ();
+			UnityEvent thisEvent = null;
+			if (eventDictionary.TryGetValue (eventName, out thisEvent)) {
+				thisEvent.Invoke ();
+			}
+		}
+
+		public static void StartEvent (string eventName)
+		{
+			init ();
+			UnityEvent thisEvent = null;
+			if (eventDictionary.TryGetValue (eventName, out thisEvent))
+			{
+				thisEvent.Invoke();
+			}
 		}
 	}
 }
