083058ea9de9bce9e29beedd7300a489cca88649
nathantaylor50 <nathantaylor50@msn.com>
2016-04-13 18:06:22 +0100

    Destroy script to destroy objects once a timer is over

--- RunnerGame/Assets/Scripts/AutoDestroyTimer.cs
+++ RunnerGame/Assets/Scripts/AutoDestroyTimer.cs
@@ -1,11 +1,32 @@
-﻿using System;
+﻿using System.Collections;
+using UnityEngine;
 
-namespace AssemblyCSharp
+namespace RunnerGame
 {
-	public class AutoDestroyTimer
+	/// <summary>
+	/// Auto destroy timer.
+	/// </summary>
+	public class AutoDestroyTimer : MonoBehaviour
 	{
-		public AutoDestroyTimer ()
+		
+		/// The timer for destruction.
+		public float TimerForDestruction = 2.0f;
+
+		/// <summary>
+		/// Start this instance.
+		/// </summary>
+		protected virtual void Start()
 		{
+			StartCoroutine (Destroy ());
+		}
+
+		/// <summary>
+		/// Destroy this instance after TimerForDestructions time
+		/// </summary>
+		protected virtual IEnumerator Destroy()
+		{
+			yield return new WaitForSeconds (TimerForDestruction);
+			Destroy (gameObject);
 		}
 	}
 }
