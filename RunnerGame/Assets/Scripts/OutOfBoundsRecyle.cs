6a05eb42a9a1b35195e6d0efed1ab2b84d193736
nathantaylor50 <nathantaylor50@msn.com>
2016-04-15 15:05:23 +0100

    object pooler

---
+++ RunnerGame/Assets/Scripts/OutOfBoundsRecyle.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AssemblyCSharp
+{
+	public class OutOfBoundsRecyle
+	{
+		public OutOfBoundsRecyle ()
+		{
+		}
+	}
+}
+
---
+++ RunnerGame/Assets/Scripts/SimpleGameObjectPooler.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AssemblyCSharp
+{
+	public class SimpleGameObjectPooler
+	{
+		public SimpleGameObjectPooler ()
+		{
+		}
+	}
+}
+
