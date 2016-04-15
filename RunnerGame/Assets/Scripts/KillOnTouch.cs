5244f1585b358a7eb79ca74e9bfc50c6d20aa66d
nathantaylor50 <nathantaylor50@msn.com>
2016-04-15 15:06:11 +0100

    kill on touch

--- RunnerGame/Assets/Scripts/KillOnTouch.cs
+++ RunnerGame/Assets/Scripts/KillOnTouch.cs
@@ -1,11 +1,37 @@
-﻿using System;
+﻿using UnityEngine;
+using System.Collections;
 
-namespace AssemblyCSharp
+namespace RunnerGame
 {
-	public class KillOnTouch
+	/// <summary>
+	/// Kill on touch.
+	/// </summary>
+	public class KillOnTouch : MonoBehaviour
 	{
-		public KillOnTouch ()
+		/// <summary>
+		/// Raises the trigger collision event.
+		/// </summary>
+		/// <param name="collider">Collider.</param>
+		protected virtual void OnTriggerCollision (Collider collider){
+
+		}
+
+		/// <summary>
+		/// Triggers the collision.
+		/// </summary>
+		/// <param name="collidingGameObject">Colliding game object.</param>
+		protected virtual void TriggerCollision (GameObject collidingGameObject)
 		{
+			//
+			if (collidingGameObject.tag != "Player") { return;}
+
+			PlayableCharacter player = collidingGameObject.GetComponent<PlayableCharacter> ();
+			if (player == null) {
+				return;
+			}
+
+			//tell the level manager to kill the player
+			LevelManager.Instance.KillPlayer(player);
 		}
 	}
 }
