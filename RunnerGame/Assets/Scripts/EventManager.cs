a7548bfe34a2513a7bbd5b55c06e27cabe4d3d9d
nathantaylor50 <nathantaylor50@msn.com>
2016-04-18 17:03:47 +0100

    Event Manager + comments

--- RunnerGame/Assets/Scripts/EventManager.cs
+++ RunnerGame/Assets/Scripts/EventManager.cs
@@ -6,17 +6,20 @@ using System.Collections.Generic;
 namespace RunnerGame
 {
 	/// <summary>
-	/// Event manager.
+	/// Event manager for handling various events
+	/// invoke these methods to tell other classes the current event
 	/// </summary>
 	public class EventManager
 	{
 		/// <summary>
-		/// The event dictionary.
+		/// The event dictionary. which represents a collection of strings 
+		/// and events
 		/// </summary>
 		private static Dictionary <string, UnityEvent> eventDictionary;
 
 		/// <summary>
-		/// Init this instance.
+		/// Initialize this instance.
+		/// if not dictionary is present make a new dictionary
 		/// </summary>
 		private static void init()
 		{
@@ -24,7 +27,11 @@ namespace RunnerGame
 				eventDictionary = new Dictionary<string, UnityEvent> ();
 			}
 		}
-
+		/// <summary>
+		/// Starts the listener.
+		/// </summary>
+		/// <param name="eventName">Event name.</param>
+		/// <param name="listener">Listener.</param>
 		public static void StartListener (string eventName,  UnityAction listener)
 		{
 			init ();
@@ -37,7 +44,11 @@ namespace RunnerGame
 				eventDictionary.Add (eventName, thisEvent);
 			}
 		}
-
+		/// <summary>
+		/// Stops the listener.
+		/// </summary>
+		/// <param name="eventName">Event name.</param>
+		/// <param name="listener">Listener.</param>
 		public static void StopListener (string eventName, UnityAction listener)
 		{
 			init ();
@@ -46,7 +57,10 @@ namespace RunnerGame
 				thisEvent.RemoveListener (listener);
 			}
 		}
-
+		/// <summary>
+		/// Starts the event.
+		/// </summary>
+		/// <param name="eventName">Event name.</param>
 		public static void StartEvent (string eventName)
 		{
 			init ();
