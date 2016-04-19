8a51d4f052f1051348cfebd4cc4059e310472de4
nathantaylor50 <nathantaylor50@msn.com>
2016-04-19 19:28:09 +0100

    Fps script for testing

---
+++ RunnerGame/Assets/Scripts/FPSCounter.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+namespace RunnerGame {
+	public class FPSCounter : MonoBehaviour
+	{
+		const float fpsMeasurePeriod = 0.5f;
+		private int fpsAccumulator;
+		private float fpsNextPeriod;
+		private int currentFps;
+		const string display = "{0} FPS";
+		Text fpsText;
+
+		private void Start()
+	{
+		fpsNextPeriod = Time.realtimeSinceStartup + fpsMeasurePeriod;
+		fpsText = GetComponent<Text> ();
+	}
+
+		private void Update()
+		{
+			//
+			fpsAccumulator++;
+			if (Time.realtimeSinceStartup > fpsNextPeriod) {
+				currentFps = (int)(fpsAccumulator / fpsMeasurePeriod);
+				fpsAccumulator = 0;
+				fpsNextPeriod += fpsMeasurePeriod;
+				fpsText.text = string.Format (display, currentFps);
+			}
+
+		}
+	}
+}
