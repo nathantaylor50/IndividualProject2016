dd477f04ebb466dcb9aff618ed64848a79fe85d9
nathantaylor50 <nathantaylor50@msn.com>
2016-04-19 06:47:02 +0100

    splash screen code for when the game starts

--- RunnerGame/Assets/Scripts/SplashScreen.cs
+++ RunnerGame/Assets/Scripts/SplashScreen.cs
@@ -1,19 +1,47 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
+using UnityEngine.SceneManagement;
 
-public class SplashScreen : MonoBehaviour
+namespace RunnerGame
 {
 
-	// Use this for initialization
-	void Start ()
-	{
-	
-	}
-	
-	// Update is called once per frame
-	void Update ()
-	{
-	
-	}
-}
 
+	/// <summary>
+	/// splash screen class
+	/// </summary>
+	public class SplashScreen : MonoBehaviour
+	{
+		public string FirstLevel;
+		public float AutoSkipDelay = 2.0f;
+		protected float delayAfterClick = 1f;
+
+		/// <summary>
+		/// Start this instance.
+		/// </summary>
+		void Start ()
+		{
+			GUIManager.Instance.FadeEffectOn (false, 1.0f);
+
+			if (AutoSkipDelay > 1.0f) {
+				delayAfterClick = AutoSkipDelay;
+				StartCoroutine (LoadFirstLevel ());
+			}
+		}
+		
+		/// <summary>
+		/// Loads the first level.
+		/// </summary>
+		/// <returns>The first level.</returns>
+		protected virtual IEnumerator LoadFirstLevel()
+		{
+			yield return new WaitForSeconds (delayAfterClick);
+			GUIManager.Instance.FadeEffectOn (true, 1.0f);
+			yield return new WaitForSeconds (1.0f);
+			SceneManager.LoadScene (FirstLevel);
+		}
+
+
+	}
+
+}
